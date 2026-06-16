49b29ee755cbfd682dd1152ca7dc97a386bb18ec
RayaneLRB <rayanelrb2001@gmail.com>
2026-06-16 18:27:24 +0100

    Delete src/utils/supabase.ts

--- src/utils/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://cjszbbxqxbiylxburrnn.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_aBynmfXirVxUkDWG8A5YJA_-3utdEob';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
